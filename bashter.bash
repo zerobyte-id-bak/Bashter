63323ab617aa7390f782801e7f77ea6733f1ef97
ZeroByte.ID Warehouse <38853171+zerobyte-id@users.noreply.github.com>
2019-05-03 03:56:03 +0700

    Update bashter.bash

--- bashter.bash
+++ bashter.bash
@@ -1,244 +1,258 @@
 #!/bin/bash
 
-BASHTER_CONFIG="/path-to/CONFIG.bash"
-
-NC=$(command -v nc)
-WGET=$(command -v wget)
-
-
-. ${BASHTER_CONFIG}
-
-if [[ ! -d ${BASHTER_HOME}/.temp ]]
-then
-	mkdir ${BASHTER_HOME}/.temp
-fi
-if [[ ! -d ${BASHTER_HOME}/.temp ]]
-then
-	echo "ERROR: Cannot make directory ${BASHTER_HOME}/.temp"
-	exit
-fi
-
-echo -ne "" > ${TMPFILE}/form-lists.tmp.bshtr
-
-function GetLinks() {
-	WEBSOURCECODE="${1}"
-	cat ${WEBSOURCECODE} | grep -o 'href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^href=["'"'"']//' -e 's/["'"'"']$//'
-	cat ${WEBSOURCECODE} | grep -o 'src=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^src=["'"'"']//' -e 's/["'"'"']$//'
-}
+HOME_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+BASHTER_VERSION="4.1"
+RELEASED_DATE="1 May 2019"
 
 function UrlSelector() {
-	WEBSOURCECODE="${1}"
+	WEBSITE="${1}"
 	URL="${2}"
-	MARK=$(echo "${3}" | sed 's/\//\\\n/g' | head -3 | tail -1 | sed 's/\\//g' | sed 's|www.||g')
-	HTTP=$(echo ${3} | sed 's|://| |g' | awk '{print $1}')
-	if [[ ! -z $(echo "${URL}" | grep -i "[.]pdf\|[.]doc\|docx\|[.]png\|[.]gif\|[.]jpg\|[.]jpeg\|[.]ico\|[.]svg\|[.]css\|[.]js\|[.]woff") ]]
+	SOURCECODE="${3}"
+	URL_SELECTOR_MODE="${4}"
+
+	if [[ ${URL_SELECTOR_MODE} == "y" ]] || [[ ${URL_SELECTOR_MODE} == "Y" ]]
+	then
+		BASESITE=$(echo "${WEBSITE}" | sed 's|/| |g' | awk '{print $2}' | awk -F '.' '{print $(NF - 1)"."$NF}')
+	else
+		BASESITE=$(echo "${WEBSITE}" | sed 's|/| |g' | awk '{print $2}')
+	fi
+
+	if [[ ! -z $(echo "${URL}" | grep ^"javascript:\|mailto:\|tel:\|data:") ]]
 	then
 		echo -ne ""
-	elif [[ $(echo ${URL} | grep "${MARK}" | grep ^'http') ]]
+	elif [[ ! -z $(echo "${URL}" | grep -i "[.]pdf\|[.]doc\|docx\|[.]png\|[.]gif\|[.]jpg\|[.]jpeg\|[.]ico\|[.]svg\|[.]css\|[.]js\|[.]woff") ]]
 	then
-		echo "[GET] ${URL}"
-	elif [[ $(echo "${URL}" | grep ^"[a-zA-Z0-9]*[:]" | grep -v 'http') ]]
-	then 
 		echo -ne ""
-	elif [[ $(echo "${URL}" | grep ^'#'$) ]]
-	then 
-		echo -ne ""
-	elif [[ $(echo "${URL}" | grep ^"[/][/][a-zA-Z0-9]") ]]
+	elif [[ ! -z $(echo "${URL}" | grep ^'//') ]]
 	then
-		if [[ $(echo "${HTTP}" | grep ^"https"$) ]] && [[ $(echo "${URL}" | grep "${MARK}") ]];
+		if [[ $(echo "${WEBSITE}" | grep ^'https://') ]] && [[ $(echo "${URL}" | grep ^"//[a-zA-Z0-9.]*.${BASESITE}") ]]
 		then
 			echo "[GET] https:${URL}"
-		elif [[ $(echo "${HTTP}" | grep ^"http"$) ]];
-		then
-			echo "[GET] ${3}$(echo "${URL}" | sed 's/^\///g')"
-		else
-			echo -ne ""
 		fi
-	elif [[ $(echo "${URL}" | grep ^"[/][a-zA-Z0-9#?]") ]]
+	elif [[ ! -z $(echo "${URL}" | grep ^"/") ]]
 	then
-		echo "[GET] ${3}${URL}"
-	elif [[ $(echo "${URL}" | grep ^"[a-zA-Z0-9]" | grep -v 'http://' | grep -v 'https://' | grep -v 'ios-app://' | grep -v 'android-app://') ]]
+		WEBSITE=$(echo "${WEBSITE}" | sed 's|://|P12O70COL|g' | sed 's|/| |g' | awk '{print $1}' | sed 's|P12O70COL|://|g')
+		FULLURL=$(echo "${WEBSITE}/${URL}" | sed 's|://|P12O70COL|g' | sed 's,/*/,/,g' | sed 's|P12O70COL|://|g')
+		echo "[GET] ${FULLURL}"
+	elif [[ ! -z $(echo "${URL}" | grep ^'?') ]]
 	then
-		CUTHERE=$(echo "${3}" | awk -F '/' '{print $NF}' | grep "[.]")
-		if [[ ! -z ${CUTHERE} ]]
-		then
-			DOMAIN=$(echo "${3}" | sed 's/\/$//g' | sed "s/${CUTHERE}//g")
-		else
-			DOMAIN=$(echo "${3}" | sed 's/\/$//g')
-		fi
-		echo "[GET] ${DOMAIN}/${URL}"
+		WEBSITE=$(echo "${WEBSITE}" | sed 's|://|P12O70COL|g' | sed 's|/| |g' | awk '{print $1}' | sed 's|P12O70COL|://|g')
+		FULLURL=$(echo "${WEBSITE}/${URL}" | sed 's|://|P12O70COL|g' | sed 's,/*/,/,g' | sed 's|P12O70COL|://|g')
+		echo "[GET] ${FULLURL}"
+	elif [[ ! -z $(echo "${URL}" | grep ^"\(http\|https\)://[a-zA-Z0-9.]*.${BASESITE}") ]]
+	then
+		echo "[GET] ${URL}"
+	elif [[ ! -z $(echo "${URL}" | grep ^'#') ]]
+	then
+		echo -ne ""
+	elif [[ ! -z $(echo "${URL}" | grep ^"[a-zA-Z0-9-]*://" | grep -v ^'http') ]]
+	then
+		echo -ne ""
 	else
 		echo -ne ""
 	fi
 }
 
+function UrlCrawler() {
+	SOURCECODE="${1}"
+	cat ${SOURCECODE} | grep -o 'href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^href=["'"'"']//' -e 's/["'"'"']$//'
+	cat ${SOURCECODE} | grep -o 'src=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^src=["'"'"']//' -e 's/["'"'"']$//'
+}
+
 function CheckForm() {
 	URL="${1}"
-	WEBSOURCECODE="${2}"
-
+	SOURCECODE="${2}"
+	PROC_ID="${3}"
+	HOME_DIR="$(cd "$(dirname "$0")" ; pwd -P)"
+	TEMPDIR="${HOME_DIR}/bashter-tempdata"
+	CHECKEDFORMFILE="${TEMPDIR}/CHECKED-FORM.BASHTER-${PROC_ID}.TMP"
 	IFS=$'\n'
-	for FORM in $(cat ${WEBSOURCECODE} | grep -Po '<form\K.*?(?=>)' | sed 's/^/<form/g' | sed 's/$/>/g')
+	for FORM in $(cat ${SOURCECODE} | grep -Po '<form\K.*?(?=>)' | sed 's/^/<form/g' | sed 's/$/>/g')
 	do
 		FORMPOST=$(echo $FORM | grep -o 'method=['"'"'"][^"'"'"']*['"'"'"]' | grep -i post)
-		if [[ $(cat ${TMPFILE}/form-lists.tmp.bshtr | grep ''$FORM'') ]]
+		if [[ ! -z $(cat ${CHECKEDFORMFILE} 2> /dev/null | grep ''$FORM'') ]]
 		then
 			echo -ne ""
 		elif [[ -z ${FORMPOST} ]]
 		then
 			echo "$(date +"[%H:%M:%S]") INFO: Form GET on \"${URL}\""
-				for FORM_TEST in $(find ${PARTSPATH}/form | grep bash$)
+			echo "$(date +"[%H:%M:%S]") FORM [GET]: \"${URL}\"" >> ${HOME_DIR}/scan-logs/${PROC_ID}-info.log
+				for FORM_TEST in $(find ${HOME_DIR}/modules/form | grep bash$)
 				do
 					if [[ ! -z ${FORM_TEST} ]]
 					then
-						bash ${FORM_TEST} ${URL} ${TMPFILE}/sourcecode.tmp.bshtr
+						bash ${FORM_TEST} ${URL} ${SOURCECODE}
 					fi
 				done
 		else 
 			echo "$(date +"[%H:%M:%S]") INFO: Form POST on \"${URL}\""
-				for FORM_TEST in $(find ${PARTSPATH}/form | grep bash$)
+			echo "$(date +"[%H:%M:%S]") FORM [POST]: \"${URL}\"" >> ${HOME_DIR}/scan-logs/${PROC_ID}-info.log
+				for FORM_TEST in $(find ${HOME_DIR}/modules/form | grep bash$)
 				do
 					if [[ ! -z ${FORM_TEST} ]]
 					then
-						bash ${FORM_TEST} ${URL} ${TMPFILE}/sourcecode.tmp.bshtr
+						bash ${FORM_TEST} ${URL} ${SOURCECODE}
 					fi
 				done
 		fi
-		echo ${FORM} >> ${TMPFILE}/form-lists.tmp.bshtr
+		echo ${FORM} >> ${CHECKEDFORMFILE}
 	done
 }
 
-########## HOME BANNER ##########
-echo ""
-cat ${BANNERFILE}
+if [[ -z ${HOME_DIR} ]]
+then
+	echo "ERROR: Can't define HOME_DIR"
+	exit
+elif [[ ! -d ${HOME_DIR} ]]
+then
+	echo "ERROR: ${HOME_DIR} not found"
+	exit
+fi
+
+cat ${HOME_DIR}/BANNER.file
 echo ""
 echo "  ##### Version ${BASHTER_VERSION} released on ${RELEASED_DATE} #####"
-echo " [ $(hostname)@BASHTER_HOME : ${BASHTER_HOME} ]"
+echo " [ $(hostname)@HOME_DIR : ${HOME_DIR} ]"
 echo ""
 
-########## INPUT HERE ##########
-echo -ne "[?] Input Website : "
+echo " Please enter the URL you want to scan..."
+echo " Example: https://website.com/[optional-path]/"
+echo -ne " >>> "
 read WEBSITE
-echo -ne "[?] Verbose mode [y/N] : "
-read VERBOSE
-VERBOSE=${VERBOSE,,}
-
+echo ""
+echo " Crawling site based on main domain or domain which you scan only"
+echo " If you want to scan *.domain.com you can enter: [Y/y]"
+echo " But If you want to scan sub.domain.com only (let it empty)"
+echo -ne " >>> "
+read URL_SELECTOR_MODE
 echo ""
 
-
-########## WEBSITE VALIDATION ##########
 if [[ -z ${WEBSITE} ]]
 then
-	echo "ERROR: Website cannot be empty!"
+	echo "ERROR: Website can't empty!"
+	exit
+elif [[ -z $(echo "${WEBSITE}" | grep ^"\(http\|https\)://") ]]
+then
+	echo "ERROR: ${WEBSITE} must using [HTTP/HTTPS]"
 	exit
 fi
-WEBSITE=${WEBSITE,,}
-if [[ ! -z $(echo ${WEBSITE} | grep ^'https://') ]]
+TEMPDIR="${HOME_DIR}/bashter-tempdata"
+if [[ ! -d ${TEMPDIR} ]]
 then
-	WEBSITE=${WEBSITE}
-elif [[ ! -z $(echo ${WEBSITE} | grep ^'http://') ]]
-then
-	WEBSITE=${WEBSITE}
-else
-	if [[ ! -z $(${NC} -vz -w 5 ${WEBSITE} 443 &> ${TMPFILE}/testconn.bshtr && cat ${TMPFILE}/testconn.bshtr | grep 'https' | grep 'succeeded' && rm ${TMPFILE}/testconn.bshtr) ]]
-	then
-		echo "$(date +"[%H:%M:%S]") INFO: ${WEBSITE} using port 443 [HTTPS]"
-		WEBSITE="https://${WEBSITE}"
-	elif [[ ! -z $(${NC} -vz -w 5 ${WEBSITE} 80 &> ${TMPFILE}/testconn.bshtr && cat ${TMPFILE}/testconn.bshtr | grep 'http' | grep 'succeeded' && rm ${TMPFILE}/testconn.bshtr) ]]
-	then
-		echo "$(date +"[%H:%M:%S]") INFO: ${WEBSITE} using port 80 [HTTP]"
-		WEBSITE="http://${WEBSITE}"
-	fi
+	mkdir ${TEMPDIR}
 fi
-if [[ -z $(${WGET} -S --spider "${WEBSITE}" 2>&1 >/dev/null | grep 'connected') ]]
+if [[ ! -d ${TEMPDIR} ]]
 then
-	echo "ERROR: Cannot connect to ${WEBSITE}"
+	echo "ERROR: Can't make directory \"${TEMPDIR}\" on \"$(pwd)\""
 	exit
 fi
-WEBSITE=$(echo "${WEBSITE}" | sed 's/\/$//g' | sed 's/\/$//g' | sed 's/\/$//g')
-echo "$(date +"[%H:%M:%S]") INFO: Scanning ${WEBSITE}..."
-if [[ ${VERBOSE} == "y" ]]
+
+PROC_ID="$(echo "${WEBSITE}" | sed 's|/| |g' | awk '{print $2}')"
+CURRENT_TEMPFILE="${TEMPDIR}/"$(echo "SOURCECODE.BASHTER-PROC_$(date +%Y%m%d%H%M%S)${RANDOM}.TMP")
+URL_LIST="${TEMPDIR}/URL-LISTS.BASHTER-${PROC_ID}.TMP"
+SCANNED_URL="${TEMPDIR}/SCANNED-URL.BASHTER-${PROC_ID}.TMP"
+
+curl -vskL --connect-timeout 5 --max-time 5 -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" "${WEBSITE}" &> ${CURRENT_TEMPFILE}
+
+if [[ ! -z $(cat ${CURRENT_TEMPFILE} | grep ^'* Could not resolve host:') ]]
 then
-	VERBOSE_FLAG="Y"
-	echo "$(date +"[%H:%M:%S]") INFO: Verbose mode activated!"
+	echo "ERROR: Could not resolve host: ${WEBSITE}"
+	exit
 fi
 
-########## TAKING SOURCE CODE AT THE FIRST TIME ##########
-curl -vskL --connect-timeout 5 --max-time 5 "${WEBSITE}" &> ${TMPFILE}/sourcecode.tmp.bshtr
-
-CheckForm ${WEBSITE} ${TMPFILE}/sourcecode.tmp.bshtr
-for HEADER_ANALYZER in $(find ${PARTSPATH}/header | grep bash$)
+echo "$(date +"[%H:%M:%S]")(Scan) ${WEBSITE}..."
+LOGNAME=$(echo "${WEBSITE}" | sed 's|/| |g' | awk '{print $2}')
+echo "$(date +"[%H:%M:%S]") URL [Scanned]: \"${WEBSITE}\"" >> ${HOME_DIR}/scan-logs/${LOGNAME}-info.log
+CheckForm "${WEBSITE}" "${CURRENT_TEMPFILE}" "${PROC_ID}"
+for HEADER_ANALYZE in $(find ${HOME_DIR}/modules/header | grep bash$)
 do
-	if [[ ! -z ${HEADER_ANALYZER} ]]
+	if [[ ! -z ${HEADER_ANALYZE} ]]
 	then
-		bash ${HEADER_ANALYZER} ${WEBSITE} ${TMPFILE}/sourcecode.tmp.bshtr
+		bash ${HEADER_ANALYZE} "${WEBSITE}" "${CURRENT_TEMPFILE}" "${PROC_ID}"
 	fi
 done
-for URL_ANALYZER in $(find ${PARTSPATH}/url | grep bash$)
+for URL_ANALYZE in $(find ${HOME_DIR}/modules/url | grep bash$)
 do
-	if [[ ! -z ${URL_ANALYZER} ]]
+	if [[ ! -z ${URL_ANALYZE} ]]
 	then
-		bash ${URL_ANALYZER} ${WEBSITE} ${TMPFILE}/sourcecode.tmp.bshtr
+		bash ${URL_ANALYZE} "${WEBSITE}" "${CURRENT_TEMPFILE}" "${PROC_ID}"
 	fi
 done
 
-if [[ -f ${TMPFILE}/url-lists.tmp.bshtr ]]
-then
-	rm ${TMPFILE}/url-lists.tmp.bshtr
-fi
-
-
-########## CRAWLING WITH LOOP ##########
-for URL in $(GetLinks ${TMPFILE}/sourcecode.tmp.bshtr)
+echo -ne "" > ${URL_LIST}
+for URL in $(UrlCrawler ${CURRENT_TEMPFILE})
 do
-	UrlSelector ${TMPFILE}/sourcecode.tmp.bshtr ${URL} ${WEBSITE} >> ${TMPFILE}/url-lists.tmp.bshtr
-	if [[ ${VERBOSE_FLAG} == "Y" ]] && [[ ! -z $(UrlSelector ${TMPFILE}/sourcecode.tmp.bshtr ${URL} ${WEBSITE}) ]]
+	URLTEMP=$(UrlSelector ${WEBSITE} ${URL} ${CURRENT_TEMPFILE} ${URL_SELECTOR_MODE} | awk '{print $2}')
+	if [[ ! -z ${URLTEMP} ]]
 	then
-		echo "$(date +"[%H:%M:%S]") $(UrlSelector ${TMPFILE}/sourcecode.tmp.bshtr ${URL} ${WEBSITE})"
+		if [[ -z $(cat ${URL_LIST} | grep "${URLTEMP}"$) ]]
+		then
+			UrlSelector ${WEBSITE} ${URL} ${CURRENT_TEMPFILE} ${URL_SELECTOR_MODE} >> ${URL_LIST}
+		fi
 	fi
 done
-echo "${WEBSITE}" > ${TMPFILE}/scanned-link.tmp.bshtr
+cat ${URL_LIST} | sort -nr | uniq > ${URL_LIST}.XTMP
+cat ${URL_LIST}.XTMP > ${URL_LIST}
+rm ${URL_LIST}.XTMP
+rm ${CURRENT_TEMPFILE}
+echo "${WEBSITE}" > ${SCANNED_URL}
 
+########## INFINITE LOOP CRAWLING ##########
 CMPCHECKPOINT=0
 while true
 do
-	for LINKS in $(cat ${TMPFILE}/url-lists.tmp.bshtr | awk '{print $2}')
+	for WEBSITE in $(cat ${URL_LIST} | awk '{print $2}')
 	do
-		if [[ -z $(cat ${TMPFILE}/scanned-link.tmp.bshtr | grep "${LINKS}") ]]
+		if [[ -z $(cat ${SCANNED_URL} | grep "${WEBSITE}"$) ]]
 		then
-			curl -vskL --connect-timeout 5 --max-time 5 "${LINKS}" &> ${TMPFILE}/sourcecode.tmp.bshtr
-
-			CheckForm ${LINKS} ${TMPFILE}/sourcecode.tmp.bshtr
-			for HEADER_ANALYZER in $(find ${PARTSPATH}/header | grep bash$)
+			echo "$(date +"[%H:%M:%S]")(Scan) ${WEBSITE}..."
+			LOGNAME=$(echo "${WEBSITE}" | sed 's|/| |g' | awk '{print $2}')
+			echo "$(date +"[%H:%M:%S]") URL [Scanned]: \"${WEBSITE}\"" >> ${HOME_DIR}/scan-logs/${LOGNAME}-info.log
+			CURRENT_TEMPFILE="${TEMPDIR}/"$(echo "SOURCECODE.BASHTER-PROC_$(date +%Y%m%d%H%M%S)${RANDOM}.TMP")
+			curl -vskL --connect-timeout 5 --max-time 5 -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0" "${WEBSITE}" &> ${CURRENT_TEMPFILE}
+			CheckForm "${WEBSITE}" "${CURRENT_TEMPFILE}" "${PROC_ID}"
+			for HEADER_ANALYZE in $(find ${HOME_DIR}/modules/header | grep bash$)
 			do
-				if [[ ! -z ${HEADER_ANALYZER} ]]
+				if [[ ! -z ${HEADER_ANALYZE} ]]
 				then
-					bash ${HEADER_ANALYZER} ${LINKS} ${TMPFILE}/sourcecode.tmp.bshtr
+					bash ${HEADER_ANALYZE} "${WEBSITE}" "${CURRENT_TEMPFILE}" "${PROC_ID}"
 				fi
 			done
-			for URL_ANALYZER in $(find ${PARTSPATH}/url | grep bash$)
+			for URL_ANALYZE in $(find ${HOME_DIR}/modules/url | grep bash$)
 			do
-				if [[ ! -z ${URL_ANALYZER} ]]
+				if [[ ! -z ${URL_ANALYZE} ]]
 				then
-					bash ${URL_ANALYZER} ${LINKS} ${TMPFILE}/sourcecode.tmp.bshtr
+					bash ${URL_ANALYZE} "${WEBSITE}" "${CURRENT_TEMPFILE}" "${PROC_ID}"
 				fi
 			done
-
-			for URL in $(GetLinks ${TMPFILE}/sourcecode.tmp.bshtr)
+			for URL in $(UrlCrawler ${CURRENT_TEMPFILE})
 			do
-				URLCHK=$(UrlSelector ${TMPFILE}/sourcecode.tmp.bshtr ${URL} ${WEBSITE} | awk '{print $2}')
-				if [[ -z $(cat ${TMPFILE}/url-lists.tmp.bshtr | grep "${URLCHK}") ]]
+				URLTEMP=$(UrlSelector ${WEBSITE} ${URL} ${CURRENT_TEMPFILE} ${URL_SELECTOR_MODE} | awk '{print $2}')
+				if [[ ! -z ${URLTEMP} ]]
 				then
-					UrlSelector ${TMPFILE}/sourcecode.tmp.bshtr ${URL} ${LINKS} >> ${TMPFILE}/url-lists.tmp.bshtr
-					if [[ ${VERBOSE_FLAG} == "Y" ]] && [[ ! -z $(UrlSelector ${TMPFILE}/sourcecode.tmp.bshtr ${URL} ${LINKS}) ]]
+					if [[ -z $(cat ${URL_LIST} | grep "${URLTEMP}"$) ]]
 					then
-						echo "$(date +"[%H:%M:%S]") $(UrlSelector ${TMPFILE}/sourcecode.tmp.bshtr ${URL} ${LINKS})"
+						UrlSelector ${WEBSITE} ${URL} ${CURRENT_TEMPFILE} ${URL_SELECTOR_MODE} >> ${URL_LIST}
 					fi
 				fi
 			done
-			echo "${LINKS}" >> ${TMPFILE}/scanned-link.tmp.bshtr
-			CMPCOMPARISON=$(cat ${TMPFILE}/scanned-link.tmp.bshtr | wc -l)
+			cat ${URL_LIST} | sort -nr | uniq > ${URL_LIST}.XTMP
+			cat ${URL_LIST}.XTMP > ${URL_LIST}
+			rm ${URL_LIST}.XTMP
+			echo "${WEBSITE}" >> ${SCANNED_URL}
+			rm ${CURRENT_TEMPFILE}
+			if [[ $(cat ${URL_LIST} | wc -l) -eq $(cat ${URL_LIST} | sort -nr | uniq | wc -l) ]]
+			then
+				echo -ne ""
+			else
+				cat ${URL_LIST} | sort -nr | uniq > ${URL_LIST}.XTMP
+				cat ${URL_LIST}.XTMP > ${URL_LIST}
+				rm ${URL_LIST}.XTMP
+				break
+			fi
 		fi
 	done
+	CMPCOMPARISON=$(cat ${SCANNED_URL} | wc -l)
 	if [[ ${CMPCHECKPOINT} -eq ${CMPCOMPARISON} ]];
 	then
 		break
@@ -246,3 +260,5 @@ do
 		CMPCHECKPOINT=${CMPCOMPARISON}
 	fi
 done
+
+rm ${SCANNED_URL}
